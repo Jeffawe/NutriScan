364a0208232164d2bfe80c16ae221042bccdc01e
Jeffery Ozoekwe-Awagu <72825145+Jeffawe@users.noreply.github.com>
2025-03-05 20:22:05 -0500

    Added Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import LandingPage from './components/pages/LandingPage';
 import SearchPage from './components/pages/SearchPage';
 import { FoodProvider } from './components/FoodContext';
 import FoodNutritionDisplay from './components/pages/NutritionDisplay';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <FoodProvider>
-      <Router>
-        <div className='w-full bg-gradient-to-b from-green-50 to-white'>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/food/:id" element={<FoodNutritionDisplay />} />
-          </Routes>
-        </div>
-      </Router>
+        <Router>
+          <div className='w-full bg-gradient-to-b from-green-50 to-white'>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/food/:id" element={<FoodNutritionDisplay />} />
+            </Routes>
+          </div>
+        </Router>
+        <Analytics />
     </FoodProvider>
   );
 }
